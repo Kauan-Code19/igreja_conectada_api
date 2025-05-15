e27bee1de205a087c9e49de746cfbc5f6775a03e
Kauan-Code19 <pkauprofissional@gmail.com>
2025-05-15 10:16:25 -0300

    primeiro commit

---
+++ src/main/java/com/kprojetos/IgrejaConectada/IgrejaConectadaApplication.java
@@ -0,0 +1,13 @@
+package com.kprojetos.IgrejaConectada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgrejaConectadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgrejaConectadaApplication.class, args);
+	}
+
+}
